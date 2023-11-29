ffe98337eec6dea507b888e658a1480b55c11ffa
Enzo Fiorelli <enzofiorelli@hotmail.com>
2023-11-29 16:26:53 -0300

    Maping Evolutions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express"); // importação do pacote
 const app = express(); 
 const axios = require("axios")
-const maxPokemons = 10;
+// const maxPokemons = 10;
 
 const pokeAPI = axios.create({
     baseURL: "https://pokeapi.co/api/v2/" //Função que guarda a API base
@@ -13,6 +13,8 @@ const porta = 3000 // Porta do servidor
 app.listen(porta, () => console.log(`O Servidor está rodando na porta: ${porta}`))
 
 app.get('/pokemon/:id', async (req, resp) => {
+
+    // ERRO NO SERVIDOR, NÃO PEGA POKEMONS A PARTIR DO ID 10
 
     const id = req.params.id; // Obtendo o valor do parâmetro :id da URL
 
@@ -42,7 +44,7 @@ app.get('/pokemon/:id', async (req, resp) => {
         // console.log('evoChain:', evoChain);
         // console.log('Gens:', gens)
         
-        console.log(pokemon)
+        // console.log(pokemon)
         return resp.json(pokemon);
 
     } catch (error) {
@@ -51,62 +53,148 @@ app.get('/pokemon/:id', async (req, resp) => {
     }   
 });
 
-
 app.get('/pokemon', async function(req, resp){
 
-    // ?limit=${maxPokemons}
+    // Paginação: Recebe o número da página e por padrão apresenta a pag 1
 
-    try {
+    // const { page = 1 } = req.query;
 
-        const { data } = await pokeAPI.get(`pokemon?offset=0&limit=50`)
+    // console.log(page)
 
-        const { data: evoChain } = await pokeAPI.get(`evolution-chain/`);
-        const { data: gens } = await pokeAPI.get(`generation/`);
-        
-            for(item in data){
+    // Limita os registros por página
+    
+    // const limit = 1;
+
+    const { data } = await pokeAPI.get(`pokemon?limit=2`)
+
+    const { data: species } = await pokeAPI.get(`pokemon-species?limit=2`)
+    // console.log(species)
+
+    let pokemons = [];
+
+    // const id = data.id
+
+    async function GetPokeBasicInfo(){
+
+        try {
+
+            for(item of data.results){
+
                 const { data } = await pokeAPI.get(item.url)
 
-                // const pokemon = {
-                //         // id:,
-                //         name: data.results.map((poke)=>{
-                //             return{name: poke.name}
-                //         }),
-                //         type: url.type.map((pokemonType) => {
-                //             return { type_name: pokemonType.type.name };
-                //         }),
-                //         // gen: gens.name,
-                //         // evo: evoChain.chain.evolves_to.map((pokemonEvo) => {
-                //         //     return{ evolves_to: pokemonEvo.species.name}
-                //         // }),
-                //         // power: data.abilities.map((pokemonAbility) => {
-                //         //     return { ability: pokemonAbility.ability.name }
-                //         // })
-                    // }
+                // console.log(data)
 
-            return resp.json(data.name)
-            return resp.json(pokemon)
-            
+                let pokemon = {
+                    id: data.id,
+                    name: data.name,
+                    type: data.types.map((pokemonType) => {
+                        return { type_name: pokemonType.type.name };
+                    }),
+                    power: data.abilities.map((pokemonAbility) => {
+                        return { ability: pokemonAbility.ability.name }
+                    }),
+                    // gens: await GetGens(),
+                    // evo: await GetEvolution()
+                } 
+
+        pokemons.push(pokemon)
+
         }
-    } catch (error) {
-        console.error(error)
-        resp.status(500).json({ error: 'Erro interno do servidor' });
+
+        // return resp.json(pokemons)
+            
+        } catch (error) {
+            console.error(error)
+            resp.status(500).json({ error: 'Erro interno do servidor' });
+        }
     }
 
+    async function GetGens(id){
+
+        try {
+
+            const { data:  species  } = await pokeAPI.get(`pokemon-species/${id}`)
+            // console.log(gens)
+            let generation = {
+                gens: species.generation.name
+            }
+
+            return generation
+            
+        } catch (error) {
+            console.error(error)
+            resp.status(500).json({ error: 'Erro interno do servidor' });
+        }
+    }
+
+    async function GetEvolution(id){
+
+        try {
+
+                const { data: species } = await pokeAPI.get(`pokemon-species/${id}`)
+            
+                const evolution_chain = await pokeAPI.get(species.evolution_chain.url)    
+
+                evoInfo = evolution_chain.data.chain;
+
+                // console.log(`Chain: `,evoInfo)
+
+                let arrayEvolutions = [evoInfo.species.name];
+
+                evolves_to = evoInfo.evolves_to
+
+                // for(item of evolves_to.results){
+                    let evolutions = {
+                        evo: JSON.stringify(evoInfo.evolves_to.map((pokemonEvo) => {
+                                return{ evolves_to: pokemonEvo.species.name}
+                            })),
+                    }
+                    // console.log(evolutions)
+
+                    arrayEvolutions.push(evolutions)
+                    console.log(arrayEvolutions)
+                // }
+            
+        } catch (error) {
+            console.error(error)
+            resp.status(500).json({ error: 'Erro interno do servidor' });
+        }
+    }
+
+    GetEvolution(1)
+
+    // await GetPokeBasicInfo()
+
+    // for(item of pokemons){
+    //     PokeGen = await GetGens(item.id)
+    //     // PokeEvolution = await GetEvolution(pokemon.id)
+    //     item.gen = PokeGen
+    //     // pokemon.PokeEvolution      
+    // }
+
+    // console.log(pokemons)
+
     
+
 })
 
 app.get('/cities', async function(req, resp){
 
-    const { data } = await pokeAPI.get(`location`);
+    try {
+
+        const { data } = await pokeAPI.get(`location`);
 
     const cities = {
         name: data.results.map((cities)=>{
             return{city: cities.name}
         })
     }
+
     return resp.json(cities)
+        
+    } catch {
+        console.error(`Erro: ${error}`);
+        resp.status(500).json({ error: 'Erro interno do servidor' }); 
+    }
 
 })
-
-
-
